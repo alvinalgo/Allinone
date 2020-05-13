cf6aa81c2ca295adbcc0d014ea2bf1325facd181
adam840502 <adam840502@gmail.com>
2020-05-13 11:04:08 +0800

    correct text format

--- dometa.py
+++ dometa.py
@@ -25,21 +25,25 @@ def findinfo(root_pth,rel_col_pth):
         print('root_pth:',root_pth)
         print('rel_col_pth:',rel_col_pth)
         pass
+    
 def makejson(somedir,info): #info:#dictionary
     submit = os.path.join(somedir,'metadata.json') #讀取回原來的檔案路徑
     with io.open(submit, 'w', encoding='utf8') as f:
         json.dump(info, f, ensure_ascii=False)
+        
 def loadmeta(somedir):
     json_filename = os.path.join(somedir,'metadata.json')
     with open(json_filename , 'r',encoding = 'utf8') as reader:
         jf = json.loads(reader.read()) #dictionary
     return(jf)
+
 def make_metadata(root_pth,rel_lib_pth):
     ing = os.path.join(root_pth,rel_lib_pth)
     folderlist = os.listdir(ing)
     for f in folderlist:
         print(f) #只會有資料夾、檔案名
         makejson(os.path.join(ing,f),findinfo(root_pth,os.path.join(rel_lib_pth,f)))
+        
 def read_metadata(root_pth,rel_lib_pth):
     base = []
     ing = os.path.join(root_pth,rel_lib_pth)
@@ -57,4 +61,3 @@ if __name__ == "__main__":
     rel_lib_pth = os.path.join('static','Collections')
     # make_metadata(root_pth,rel_lib_pth)
     base = read_metadata(root_pth,rel_lib_pth)
-
--- main.py
+++ main.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 app.config['folder'] = 'static'
 # app.config['folder'] = '/'
 
-#網頁們------------------------------------------------------
+# 網頁們 ------------------------------------------------------
 @app.route('/')
-def home(): #再來要把圖片做超連結
+def home():    # 再來要把圖片做超連結
     covinfo = []
     for coll in base:
         covinfo.append(coll["cover_img"])
-    # return render_template('home.html', covinfo = covinfo, title='This is home')
-    return render_template('index.html', covinfo = covinfo, title='This is home')
+    # return render_template('home.html', covinfo=covinfo, title='This is home')
+    return render_template('index.html', covinfo=covinfo, title='This is home')
 
 @app.route('/test')
 def test():
@@ -36,15 +36,12 @@ def test():
 @app.route('/<cname>')
 def pg2(cname):
     # r = 'static\\Collections\\FBIMG'
-    r = os.path.join(app.config['folder'],'Collections',cname)
-    return render_template('page2.html', colinfo = do.findinfo(root_pth,r), title='子網頁') #colinfo 是個dictionary
+    r = os.path.join(app.config['folder'], 'Collections', cname)
+    return render_template('page2.html', colinfo=do.findinfo(root_pth,r), title='子網頁') # colinfo 是個 dictionary
 
 if __name__ == "__main__":
     root_pth = 'C:\\Users\\kurogikeikan\\codes\\Bootstrap project\\主程式'
-    rel_lib_pth = os.path.join('static','Collections')
-    do.make_metadata(root_pth,rel_lib_pth)
-    base = do.read_metadata(root_pth,rel_lib_pth)
+    rel_lib_pth = os.path.join('static', 'Collections')
+    do.make_metadata(root_pth, rel_lib_pth)
+    base = do.read_metadata(root_pth, rel_lib_pth)
     app.run(debug=True)
-
-
-
